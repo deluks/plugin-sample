5ef268bbadd292c9395f2ac6390efe208d71f877
deluks <iamalux@gmail.com>
2015-08-01 23:00:44 +0200

    init

--- plugin.php
+++ plugin.php
@@ -1,25 +1,27 @@
 <?php
 /*
-Plugin Name: Your Plugin Name
-Plugin URI: http://github.com/your_name/your_plugin
-Description: One line description of your plugin
+Plugin Name: bURL
+Plugin URI: http://github.com/deluks/burl
+Description: Includes original domainname and file extension in the shortened link
 Version: 1.0
-Author: Your Name
+Author: Lukas Lindinger
 Author URI: http://your-site-if-any/
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-/*
+global $deluks_burl_domainname;
+global $deluks_burl_fileextension;
 
- Your code goes here.
- 
- Suggested read:
- https://github.com/YOURLS/YOURLS/wiki/Coding-Standards
- https://github.com/YOURLS/YOURLS/wiki#for-developpers
- https://github.com/YOURLS/YOURLS/wiki/Plugin-List#get-your-plugin-listed-here
- 
- Have fun!
- 
-*/
+function deluks_burl( $url, $keyword, $title ) {
+    $deluks_burl_domainname = parse_url($url, PHP_URL_HOST);
+    $deluks_burl_fileextension = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION); // should be ".ext" or empty!
+}
+
+function deluks_burl_keyword ( $keyword, $url, $title ) {
+    return $deluks_burl_domainname . "/" . $keyword . $deluks_burl_fileextension;
+}
+
+yourls_do_action( 'pre_add_link', 'deluks_burl' );
+yourls_add_filter( 'random_keyword', 'deluks_burl_keyword' );
