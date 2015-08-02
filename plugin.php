1c4337b5651c936f43db95d11554fbdcfe4547d4
deluks <iamalux@gmail.com>
2015-08-02 20:18:00 +0200

    some commentary

--- plugin.php
+++ plugin.php
@@ -5,16 +5,21 @@ Plugin URI: http://github.com/deluks/burl
 Description: Includes original domainname and file extension in the shortened link
 Version: 1.0
 Author: Lukas Lindinger
-Author URI: http://your-site-if-any/
+Author URI: http://deluks.de/
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
 function deluks_burl_keyword ( $keyword, $url, $title ) {
+    /* get domain of the original url */
     $domainname = parse_url($url, PHP_URL_HOST);
+
+    /* get file extension of original url */
     $file_extension = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION);
-    if (!$file_extension) { $file_extension .= "."; }
+
+    /* if there was a file extension, put a "." in front of it */
+    if (!empty($file_extension)) { $file_extension = "." . $file_extension; }
 
     return $domainname . "/" . $keyword . $file_extension;
 }
