408c6a1b31c0e91e478ceede73db2aa8cfa3ebe7
deluks <iamalux@gmail.com>
2015-08-02 17:49:02 +0200

    really only one function needed

--- plugin.php
+++ plugin.php
@@ -11,17 +11,12 @@ Author URI: http://your-site-if-any/
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-global $deluks_burl_domainname;
-global $deluks_burl_fileextension;
-
-function deluks_burl( $url, $keyword, $title ) {
-    $deluks_burl_domainname = parse_url($url, PHP_URL_HOST);
-    $deluks_burl_fileextension = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION); // should be ".ext" or empty!
-}
-
 function deluks_burl_keyword ( $keyword, $url, $title ) {
-    return $deluks_burl_domainname . "/" . $keyword . $deluks_burl_fileextension;
+    $domainname = parse_url($url, PHP_URL_HOST);
+    $file_extension = pathinfo(parse_url($url, PHP_URL_PATH), PATHINFO_EXTENSION);
+    if (!$file_extension) { $file_extension .= "."; }
+
+    return $domainname . "/" . $keyword . $file_extension;
 }
 
-yourls_do_action( 'pre_add_link', 'deluks_burl' );
 yourls_add_filter( 'random_keyword', 'deluks_burl_keyword' );
